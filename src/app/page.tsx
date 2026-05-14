26ac89dedc8edc1a166effea51b61442692b1d0c
Kim dong tak <59105154+Dong-tak@users.noreply.github.com>
2026-05-14 19:58:09 +0900

    Update Jemigong landing media flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import BrandStorySection from "@/components/BrandStorySection";
 import ServiceSection from "@/components/ServiceSection";
-import PricingSection from "@/components/PricingSection";
+import MediaSection from "@/components/MediaSection";
 import ProductsSection from "@/components/ProductsSection";
-import ReviewSection from "@/components/ReviewSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
@@ -13,11 +12,10 @@ export default function Home() {
     <main>
       <Header />
       <HeroSection />
+      <MediaSection />
+      <ProductsSection />
       <BrandStorySection />
       <ServiceSection />
-      <PricingSection />
-      <ProductsSection />
-      <ReviewSection />
       <ContactSection />
       <Footer />
     </main>
